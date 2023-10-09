497ddc4eeb21e546068e6c2830aacfce3776e48e
Aanchal Singh <aanchalsinghuk@gmail.com>
2023-10-09 08:38:40 +0530

    l

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,29 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Dash.css";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:4000"); // Replace with your server's address
 
 function Dashboard() {
+  const [inputText, setInputText] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    socket.on("chat message", (message) => {
+      setMessages([...messages, message]);
+    });
+
+    return () => {
+      socket.off("chat message");
+    };
+  }, [messages]);
+
+  const handleSendMessage = (e) => {
+    if (inputText.trim() !== "") {
+      socket.emit("chat message", {
+        text: inputText,
+        sender: "Arya",
+      });
+    }
+
+    if (e.key === "Enter" || e.type === "click") {
+      const messageInput = document.getElementById("messageInput");
+      const message = messageInput.value;
+      if (message.trim() !== "") {
+        setMessages([...messages, { text: message, sender: "Arya" }]);
+        messageInput.value = "";
+      }
+      setInputText("");
+    }
+  };
   return (
-    <div className="chat-app-dashboard">
-      <div className="blue-portion">
-        <center>
-        <img src="https://cdn3d.iconscout.com/3d/premium/thumb/young-boy-6877456-5638292.png?f=webp" alt=""style={{borderRadius:"100%",backgroundColor:"white",marginTop:"5%"}} height={120}width={120}/>
-        <h2 style={{color:"white"}}>Arya</h2>
-        <br />
-        <hr style={{color:"violet"}} />
-        </center>
-      </div>
-      <div className="gray-portion">
-        <div className="chat-messages">
-        </div>
-        <div className="chat-input">
+    <div className="home-container">
+      <div id="a1">
+        <img
+          src="pro.jpg"
+          alt=""
+          style={{
+            height: "20vh",
+            width: "10vw",
+            borderRadius: "100%",
+            position: "fixed",
+            top: "3%",
+            left: "3%",
+          }}
+        />
+        <div id="a2">
+          <button className="chat-logout" onClick={logout}>
+            <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+              Logout
+            </Link>
+          </button>
+          <div className="chat-box" id="chatBox">
+            {messages.map((message, index) => (
+              <div key={index} className="message-box">
+                {message.text}
+              </div>
+            ))}
+          </div>
           <input
             type="text"
-            placeholder="Type your message..."
+            className="input-box"
+            id="messageInput"
+            placeholder="Type a message..."
+            onKeyDown={handleSendMessage}
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
           />
-          <button>Send</button>
+          <button className="chat-input" onClick={handleSendMessage}>
+            Send
+          </button>
         </div>
       </div>
     </div>
   );
 }
+
 export default Dashboard;
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getAuth ,GoogleAuthProvider} from "firebase/auth";
+import { initializeApp} from "firebase/app";
+import { getDatabase} from "firebase/database";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig={
     apiKey: "AIzaSyAMB1Vdj3-mHKB06yAx9AE3NnqE9vBOFqI",
@@ -7,10 +8,13 @@ const firebaseConfig={
     projectId: "authentication-a2981",
     storageBucket: "authentication-a2981.appspot.com",
     messagingSenderId: "266110574101",
-    appId: "1:266110574101:web:66e53120c152b02b51381c"
+    appId: "1:266110574101:web:66e53120c152b02b51381c",
+    databaseURL:
+        "https://authentication-a2981-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const app=initializeApp(firebaseConfig);
+const auth=getAuth(app);
+const database = getDatabase(app);
 const provider=new GoogleAuthProvider();
 
-export {auth,provider};
+export { auth, provider,database};
